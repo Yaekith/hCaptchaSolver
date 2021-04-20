5dc22c3438cd7e0cf2ef05e5073535a707f1c118
Yaekith <samsscopes2@gmail.com>
2021-04-20 15:53:41 +1000

    Update HCaptcha.cs

--- HCaptcha-Solver/API/HCaptcha.cs
+++ HCaptcha-Solver/API/HCaptcha.cs
@@ -42,6 +42,9 @@ namespace HCaptcha_Solver.API
             Client = new HttpClient();
             YoloClient = new Yolo();
             Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36");
+
+            if (!Directory.Exists("Images"))
+                Directory.CreateDirectory("Images");
         }
 
         private HCaptchaSiteConfig GetSiteConfig()
